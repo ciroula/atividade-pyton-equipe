9c86859e2bd33a705fedcaa11d842427c9572a86
ciroula <159503125+ciroula@users.noreply.github.com>
2024-09-30 14:09:23 +0000

    engenheiro

---
+++ projeto/models/engenheiro.py
@@ -0,0 +1,12 @@
+from projeto.models.endereco import Endereco
+from projeto.models.funcionario import Funcionario
+
+class Engeneheiro(Funcionario):
+    def __init__(self, nome: str, telefone: str, email: str, endereco: Endereco, crea: str) -> None:
+        super().__init__(nome, telefone, email, endereco)
+        self.crea = crea 
+
+    def __str__(self) -> str:
+        return (f"{super().__str__()}"
+                f"CREA: {self.crea}"
+                )
