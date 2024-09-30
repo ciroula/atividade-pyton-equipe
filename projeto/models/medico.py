5c7e0f7fe008daea091dc48bfc417237030324c5
pedroper2 <161596207+pedroper2@users.noreply.github.com>
2024-09-30 13:39:03 +0000

    atividade

---
+++ projeto/models/medico.py
@@ -0,0 +1,9 @@
+from projeto.models.endereco import Endereco
+from projeto.models.funcionario import Funcionario
+class Medico(Funcionario):
+    def __init__(self, nome: str, telefone: str, email: str, crm:str,endereco: Endereco) -> None:
+        self.crm= crm
+        super().__init__(nome, telefone, email, endereco)
+    def __str__(self) -> str:
+        return super().__str__() + f"\ncrm{self.crm}"
+    
